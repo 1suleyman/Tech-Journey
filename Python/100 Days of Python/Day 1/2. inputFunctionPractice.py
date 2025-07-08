0ede1f171d4445205d8b8e58bedba8f37c8520d4
1suleyman <110779287+1suleyman@users.noreply.github.com>
2025-07-08 17:12:27 +0100

    Create 2. inputFunctionPractice.py

---
+++ Python/100 Days of Python/Day 1/2. inputFunctionPractice.py
@@ -0,0 +1,10 @@
+print("Hello " + input("What is your name?")+ "!")
+
+# 🔄 What’s Happening Under the Hood:
+# Python shows: "What is your name?"
+
+# You type your name → "Suleyman"
+
+# Python runs: "Hello " + "Suleyman"
+
+# It prints: Hello Suleyman!
