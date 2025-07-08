d79d87bc2d9a439dbc00c3cb44fd5cdb0a67f2c8
1suleyman <110779287+1suleyman@users.noreply.github.com>
2025-07-08 17:15:13 +0100

    Create TipCalculator.py

---
+++ Python/100 Days of Python/Day 2/Project Tip Calculator/TipCalculator.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+bill = int(input("What was the total bill? "))
+tip = int(input("What percentage tip would you like to give? 10, 12 or even 15 "))
+people = int(input("How many people want to split the bill? "))
+final_bill = round(bill * (1 + (tip) / 100) / people, 2)
+print(f"Each person should pay: ${final_bill}")
