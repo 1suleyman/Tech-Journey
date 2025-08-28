d9ce5dbced4c6a5c94933626bc3da8861c37a1f7
1suleyman <110779287+1suleyman@users.noreply.github.com>
2025-08-28 11:37:42 +0100

    Update 1. Debugging Odd or Even.py

--- Python/100 Days of Python/Day 13/1. Debugging Odd or Even.py
+++ Python/100 Days of Python/Day 13/1. Debugging Odd or Even.py
@@ -1,1 +1,14 @@
-h
+# def odd_or_even(number):
+#     if number % 2 = 0:
+#         return "This is an even number."
+#     else:
+#         return "This is an odd number."
+    
+# turns into 
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+
+print(odd_or_even(3))
