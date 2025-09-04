4c2836cb857e14d9a61a73fb96c837e83f2a533e
1suleyman <110779287+1suleyman@users.noreply.github.com>
2025-09-04 15:18:30 +0100

    Update CoffeeMachine.py

--- Python/100 Days of Python/Day 15/CoffeeMachine.py
+++ Python/100 Days of Python/Day 15/CoffeeMachine.py
@@ -32,15 +32,70 @@ resources = {
 
 print("Welcome to the coffee machine!")
 
-shoudl_continue = True
+should_continue = True
 
 quarter_value = 0.25
 dime_value = 0.10
 nickel_value = 0.05
 penny_value = 0.01
 
+# make totlal money variable outside the loop
+total_money = 0
 
-while shoudl_continue:
+# create a function that deducts the espresso ingredients from the resources
+def deduct_espresso():
+    resources["water"] -= MENU["espresso"]["ingredients"]["water"]
+    # print the water that is left
+    print(f"Water left: {resources['water']}ml")
+    resources["coffee"] -= MENU["espresso"]["ingredients"]["coffee"]
+    # print the coffee that is left
+    print(f"Coffee left: {resources['coffee']}g")
+
+# create a function that deducts the latte ingredients from the resources
+def deduct_latte():
+    resources["water"] -= MENU["latte"]["ingredients"]["water"]
+    # print the water that is left
+    print(f"Water left: {resources['water']}ml")
+    resources["milk"] -= MENU["latte"]["ingredients"]["milk"]
+    # print the milk that is left
+    print(f"Milk left: {resources['milk']}ml")
+    resources["coffee"] -= MENU["latte"]["ingredients"]["coffee"]
+    # print the coffee that is left
+    print(f"Coffee left: {resources['coffee']}g")
+
+# create a function that deducts the cappuccino ingredients from the resources
+def deduct_cappuccino():
+    resources["water"] -= MENU["cappuccino"]["ingredients"]["water"]
+    # print the water that is left
+    print(f"Water left: {resources['water']}ml")
+    resources["milk"] -= MENU["cappuccino"]["ingredients"]["milk"]
+    # print the milk that is left
+    print(f"Milk left: {resources['milk']}ml")
+    resources["coffee"] -= MENU["cappuccino"]["ingredients"]["coffee"]
+    # print the coffee that is left
+    print(f"Coffee left: {resources['coffee']}g")
+
+# check if there are enough resources to make the drink using this function
+def check_resources(drink):
+    can_make = True
+    for item in drink["ingredients"]:
+        if drink["ingredients"][item] > resources[item]:
+            print(f"Sorry, there is not enough {item}.")
+            can_make = False
+        if not can_make:
+            continue
+
+# create a function to process coins
+def process_coins():
+    print("Please insert coins.")
+    total = 0
+    total += int(input("How many quarters?: ")) * quarter_value
+    total += int(input("How many dimes?: ")) * dime_value
+    total += int(input("How many nickels?: ")) * nickel_value
+    total += int(input("How many pennies?: ")) * penny_value
+    return total
+
+while should_continue:
     # show the user the worth of each american coin that can be used
     print("Here is the worth of each coin you can use:")
     print("Penny: $0.01")
@@ -60,11 +115,8 @@ while shoudl_continue:
         print(f"Water: {resources['water']}ml")
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
-        # calculate the total money in the machine by adding up the cost of each drink made
-        total_money = 0
-        for drink in MENU:
-            total_money += MENU[drink]["cost"]
-        print(f"Money: ${total_money}")
+        # also print the total money earned by the machine
+        print(f"The machine currently holds ${total_money}")
         continue
     # check if the user choice is valid
     elif userchoice not in MENU:
@@ -73,11 +125,56 @@ while shoudl_continue:
     # check if user a chosen a drink in the menu
     elif userchoice == "espresso":
         drink = MENU["espresso"]
+        check_resources(drink)
+
+        # see if the user has inserted enough money to buy the drink
+        money_received = process_coins()
+        if money_received < drink["cost"]:
+            print("Sorry, that's not enough money. Money refunded.")
+            total_money -= MENU["espresso"]["cost"]
+            continue
+        elif money_received > drink["cost"]:
+            change = round(money_received - drink["cost"], 2)
+            print(f"Here is ${change} in change.")
+            # add the cost of the drink to the total money
+            total_money += MENU["cappuccino"]["cost"]
+
+        # deduct the ingredients from the resources
+        deduct_espresso()
     elif userchoice == "latte":
         drink = MENU["latte"]
+        check_resources(drink)
+
+        # see if the user has inserted enough money to buy the drink
+        money_received = process_coins()
+        if money_received < drink["cost"]:
+            print("Sorry, that's not enough money. Money refunded.")
+            total_money -= MENU["espresso"]["cost"]
+            continue
+        elif money_received > drink["cost"]:
+            change = round(money_received - drink["cost"], 2)
+            print(f"Here is ${change} in change.")
+            # add the cost of the drink to the total money
+            total_money += MENU["cappuccino"]["cost"]
+
+        deduct_latte()
     elif userchoice == "cappuccino":
         drink = MENU["cappuccino"]
+        check_resources(drink)
+        
+        # see if the user has inserted enough money to buy the drink
+        money_received = process_coins()
+        if money_received < drink["cost"]:
+            print("Sorry, that's not enough money. Money refunded.")
+            total_money -= MENU["espresso"]["cost"]
+            continue
+        elif money_received > drink["cost"]:
+            change = round(money_received - drink["cost"], 2)
+            print(f"Here is ${change} in change.")
+            # add the cost of the drink to the total money
+            total_money += MENU["cappuccino"]["cost"]
+
+        deduct_cappuccino()
     else:
         print("Invalid choice. Please choose again.")
         continue
-    # check if there are enough resources to make the drink
