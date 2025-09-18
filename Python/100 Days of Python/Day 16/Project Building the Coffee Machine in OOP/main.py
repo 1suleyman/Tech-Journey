aa6375ece69a66bd866c78d3ea84d0204fa45a4b
1suleyman <110779287+1suleyman@users.noreply.github.com>
2025-09-18 11:41:24 +0100

    Update main.py

--- Python/100 Days of Python/Day 16/Project Building the Coffee Machine in OOP/main.py
+++ Python/100 Days of Python/Day 16/Project Building the Coffee Machine in OOP/main.py
@@ -1,3 +1,28 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+ 
+print("Welcome to the Coffee Machine!")
+
+# Create objects
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+# Create a variable to control the while loop
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+    
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if drink and coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
